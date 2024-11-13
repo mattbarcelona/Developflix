8029ca85563dea6261757cc5cf6fe7844df4266b
mattbarcelona <mathieu.driencourt@yahoo.fr>
2024-11-13 09:55:51 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,51 @@
 import peliculas from "./peliculas.js";
+let accion = document.getElementById("genero-28");
+let thriller = document.getElementById("genero-53");
+let aventura = document.getElementById("genero-12");
 
-let developflix = () => {
-  let accion = document.getElementById("genero-28");
-  let thriller = document.getElementById("genero-53");
-  let aventura = document.getElementById("genero-12");
-  let accionMovies = [];
-  accionMovies = peliculas
-    .filter((movie) => movie.genre_ids.includes(28))
-    .map((movie) => movie.poster_path && movie.title);
-  accion.innerHTML = accionMovies;
+const imageBase = "https://image.tmdb.org/t/p/w500";
 
-  let thrillerMovies = [];
-  thrillerMovies = peliculas
-    .filter((movie) => movie.genre_ids.includes(53))
-    .map((movie) => movie.title);
-  thriller.innerHTML = thrillerMovies;
+let accionMovies = peliculas
+  .filter((movie) => movie.genre_ids.includes(28))
+  .map((movie) => ({
+    poster_path: imageBase.concat(movie.poster_path),
+    title: movie.title,
+  }));
+console.log(accionMovies);
 
-  let aventuraMovies = [];
-  aventuraMovies = peliculas
-    .filter((movie) => movie.genre_ids.includes(12))
-    .map((movie) => movie.title);
-  aventura.innerHTML = aventuraMovies;
-};
-developflix();
+let thrillerMovies = peliculas
+  .filter((movie) => movie.genre_ids.includes(53))
+  .map((movie) => ({
+    poster_path: imageBase.concat(movie.poster_path),
+    title: movie.title,
+  }));
+console.log(thrillerMovies);
+
+let aventuraMovies = peliculas
+  .filter((movie) => movie.genre_ids.includes(12))
+  .map((movie) => ({
+    poster_path: imageBase.concat(movie.poster_path),
+    title: movie.title,
+  }));
+console.log(aventuraMovies);
+
+function orderMovies(pelicula, genList) {
+  pelicula.forEach((element) => {
+    const divGen = document.createElement("div");
+    const titleGen = document.createElement("h3");
+    titleGen.textContent = element.title;
+    titleGen.classList.add("title");
+
+    const imgGen = document.createElement("img");
+    imgGen.src = element.poster_path;
+    imgGen.alt = element.title;
+
+    divGen.appendChild(titleGen);
+    divGen.appendChild(imgGen);
+
+    genList.appendChild(divGen);
+  });
+}
+orderMovies(accionMovies, accion);
+orderMovies(thrillerMovies, thriller);
+orderMovies(aventuraMovies, aventura);
